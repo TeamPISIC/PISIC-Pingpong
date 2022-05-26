8faf534a0000fde61f9416255b9e9a596c843fe6
juhyeon <wngus4296@swu.ac.kr>
2022-05-26 14:41:40 +0900

    [FEAT] csv 파일 읽어서 답변 자동으로 나오는 기능 구현

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,8 @@
 import PingPongWr
 import asyncio
-
 from dotenv import load_dotenv
 import os
+import csv
 
 load_dotenv()
 
@@ -12,13 +12,17 @@ pingpong_token = os.environ.get('Authorization')
 # 핑퐁 모듈 클래스 선언
 Ping = PingPongWr.Connect(url, pingpong_token)
 
+# 질문 데이터 가져오기
+f = open('sampleData.csv', 'r', encoding='utf-8-sig')
+rdr = csv.reader(f)
+
 
 async def Example():
-    while True:
-        str_text = input("입력: ")
-        if str_text == "끝내기":
-            break
+    for str_text in rdr:
+        print(str_text)
         return_data = await Ping.Pong(session_id="Example", text=str_text, topic=False, image=False, dialog=False)
         print("피식이: " + return_data.get('text'))
 
 asyncio.run(Example())
+
+f.close()
