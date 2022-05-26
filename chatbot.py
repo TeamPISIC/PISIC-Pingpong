a567ab028e2f4164e2d1b7e0234e5284282112d0
juhyeon <wngus4296@swu.ac.kr>
2022-05-26 14:22:19 +0900

    [FEAT] pingpong 챗봇 api 연결 구현

--- chatbot.py
+++ chatbot.py
@@ -1,25 +1,24 @@
-import requests
+import PingPongWr
+import asyncio
 
-# 1. 헤더설정
-header = {
-    'Authorization': 'Basic a2V5OjRlYzg4MDBiOWIwMGRjOTBkODc3NGYwMDk2YTMzNmNl',  # 본인의 인증키를 적어야겠죠?
-    'Content-Type': "application/json",
-}
+from dotenv import load_dotenv
+import os
 
-# 2. Request Body 설정
-param = {
-    "request": {
-        "query": "안녕하세요"
-    }
-}
+load_dotenv()
 
-# 3. 주소설정
-sessionId = "mySessionId"  # 본인의 Session Id를 적어주세요
-url = 'https://builder.pingpong.us/api/builder/5ebe0072e4b0e921afb5c210/integration/v0.2/custom/' + sessionId
+url = os.environ.get('URL')
+pingpong_token = os.environ.get('Authorization')
 
-# 설정 끝 Request 날리기
-req = requests.post(url, data=param, headers=header)
+# 핑퐁 모듈 클래스 선언
+Ping = PingPongWr.Connect(url, pingpong_token)
 
-# Response 확인하기
-data = req.json()
-print(data)
+
+async def Example():
+    while True:
+        str_text = input("입력: ")
+        if str_text == "끝내기":
+            break
+        return_data = await Ping.Pong(session_id="Example", text=str_text, topic=False, image=False, dialog=False)
+        print("피식이: " + return_data.get('text'))
+
+asyncio.run(Example())
